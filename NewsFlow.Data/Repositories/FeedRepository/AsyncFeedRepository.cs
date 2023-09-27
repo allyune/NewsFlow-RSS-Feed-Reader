28c0e07de80c9f7d96d47ba86d04fff277e93ce7
polinakrasnianska <p.krasniansky@gmail.com>
2023-09-27 11:23:08 +0200

    #2: Implement Feed repository:

---
+++ NewsFlow.Data/Repositories/FeedRepository/AsyncFeedRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using NewsFlow.Data.Infrastructure;
+using NewsFlow.Data.Models;
+
+namespace NewsFlow.Data.Repositories.FeedRepository
+{
+    public class AsyncFeedRepository : AsyncRepository<Feed>, IAsyncFeedRepository
+    {
+        private readonly RssDbContext _dbContext;
+        private readonly ILogger<AsyncFeedRepository> _logger;
+
+        public AsyncFeedRepository(
+            RssDbContext dbContext, ILogger<AsyncFeedRepository> logger)
+            : base(dbContext)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+    }
+}
+
