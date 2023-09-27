b5f96eba8a44db65143adb6cced4b141ee24a4dd
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-09-27 20:26:33 +0200

    #8: Add article mapper

---
+++ NewsFlow.Application/Mapping/ArticleMapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+using NewsFlow.Domain.Entities;
+
+namespace NewsFlow.Application.Mapping
+{
+	public interface IArticleMapper
+    {
+		public Article FeedItemToArticle(SyndicationItem item);
+	}
+}
