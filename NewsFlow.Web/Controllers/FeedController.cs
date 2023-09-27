e38d764b53b089b95d253c9e5c09589e3cf8956e
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-09-27 13:10:18 +0200

    #8: Define Feed controller endpoints

---
+++ NewsFlow.Web/Controllers/FeedController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewsFlow.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class FeedsController : Controller
+    {
+        private readonly ILogger<FeedsController> _logger;
+
+        public FeedsController(ILogger<FeedsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListFeeds()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{feedId}")]
+        public async Task<IActionResult> LoadFeedDetails(Guid feedId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddFeed([FromBody] AddFeedDto data)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{feedId}")]
+        public async Task<IActionResult> DeleteFeed(Guid feedId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{feedId}")]
+        public async Task<IActionResult> LoadFeedArticles(Guid feedId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
+
