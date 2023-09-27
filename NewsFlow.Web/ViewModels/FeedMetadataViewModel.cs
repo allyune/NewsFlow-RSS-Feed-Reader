f16e00adbad069d6fec170ecb7f8cab09edb2d97
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-09-27 13:39:33 +0200

    #8: Add FeedMetadata view model

---
+++ NewsFlow.Web/ViewModels/FeedMetadataViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace NewsFlow.Web.ViewModels
+{
+	public class FeedMetadataViewModel
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		public FeedMetadataViewModel(string name, string description)
+		{
+			Name = name;
+			Description = description;
+		}
+	}
+}
+
