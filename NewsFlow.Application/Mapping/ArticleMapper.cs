6ae85ecb46d6d7e373a9387a80f42365edf4053c
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-09-27 22:12:52 +0200

    #8: Fix naming of the Mapper files

--- NewsFlow.Application/Mapping/ArticleMapper.cs
+++ NewsFlow.Application/Mapping/ArticleMapper.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-using System.ServiceModel.Syndication;
 using NewsFlow.Domain.Entities;
+using System.ServiceModel.Syndication;
 
 namespace NewsFlow.Application.Mapping
 {
-	public interface IArticleMapper
+    public class ArticleMapper : IArticleMapper
     {
-		public Article FeedItemToArticle(SyndicationItem item);
-	}
+        public Article FeedItemToArticle(SyndicationItem item)
+        {
+            return Article.Create(
+                    item.Id,
+                    item.Title.Text,
+                    item.Summary.Text,
+                    item.PublishDate.Date,
+                    item.Links.Select(l => l.Uri.AbsoluteUri).ToList(),
+                    item.Authors.Select(a => a.Name).ToList(),
+                    item.Categories.Select(c => c.Name).ToList());
+        }
+    }
 }
--- NewsFlow.Application/Mapping/IArticleMapper.cs
+++ NewsFlow.Application/Mapping/IArticleMapper.cs
@@ -1,22 +1,11 @@
 ﻿using System;
-using NewsFlow.Domain.Entities;
 using System.ServiceModel.Syndication;
+using NewsFlow.Domain.Entities;
 
 namespace NewsFlow.Application.Mapping
 {
-    public class ArticleMapper : IArticleMapper
+	public interface IArticleMapper
     {
-        public Article FeedItemToArticle(SyndicationItem item)
-        {
-            return Article.Create(
-                    item.Id,
-                    item.Title.Text,
-                    item.Summary.Text,
-                    item.PublishDate.Date,
-                    item.Links.Select(l => l.Uri.AbsoluteUri).ToList(),
-                    item.Authors.Select(a => a.Name).ToList(),
-                    item.Categories.Select(c => c.Name).ToList());
-        }
-    }
+		public Article FeedItemToArticle(SyndicationItem item);
+	}
 }
-
