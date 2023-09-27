e879c12a783961501c63f86d232ffd8b7c8b44a1
polinakrasnianska <p.krasniansky@gmail.com>
2023-09-27 09:44:50 +0200

    #2: Register DbContext with the main project

--- NewsFlow.Web/Program.cs
+++ NewsFlow.Web/Program.cs
@@ -1,7 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using NewsFlow.Data.NewsFlowDbContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.Development.json");
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<RssDbContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("RssDbConnectionString")));
 
 var app = builder.Build();
 
