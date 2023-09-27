c425fd0d66f2122a7ca262245524fc9b0e3fa751
polinakrasnianska <p.krasniansky@gmail.com>
2023-09-27 11:13:13 +0200

    #2: Fix DbContext namespace in migrations

--- NewsFlow.Data/Migrations/20230927075038_Feed.Designer.cs
+++ NewsFlow.Data/Migrations/20230927075038_Feed.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using NewsFlow.Data.NewsFlowDbContext;
+using NewsFlow.Data.Infrastructure;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
