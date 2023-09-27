ffe37f4f11a25698660d85d6819dc59015d84a68
polinakrasnianska <p.krasniansky@gmail.com>
2023-09-27 09:42:35 +0200

    #2: Fix Default timestamp function for Feed model

---
+++ NewsFlow.Data/NewsFlowDbContext/RssDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Design;
+using NewsFlow.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace NewsFlow.Data.NewsFlowDbContext
+{
+    public class RssDbContext : DbContext
+    {
+
+        public RssDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Feed>()
+                .HasKey(f => f.Id);
+
+            modelBuilder.Entity<Feed>()
+                .Property(f => f.TimestampAdded)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            modelBuilder.Entity<Feed>()
+                .HasIndex(f => f.Link)
+                .IsUnique();
+        }
+
+        public DbSet<Feed> Users { get; set; }
+    }
+}
+
