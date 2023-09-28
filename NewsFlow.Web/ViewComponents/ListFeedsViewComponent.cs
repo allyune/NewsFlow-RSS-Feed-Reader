3cf1d59e50a4b9e60c91473a6d2f1e14ae806254
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-09-28 22:42:17 +0200

    #11: Define list of Feeds as a ViewComponent + basic HTML structure

---
+++ NewsFlow.Web/ViewComponents/ListFeedsViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NewsFlow.Application.UseCases.AddFeeds;
+using NewsFlow.Application.UseCases.DeleteFeeds;
+using NewsFlow.Application.UseCases.LoadFeeds;
+using NewsFlow.Web.Controllers;
+using NewsFlow.Web.Mapping.FeedMapping;
+using NewsFlow.Web.ViewModels;
+using Models = NewsFlow.Data.Models;
+
+namespace NewsFlow.Web.ViewComponents
+{
+	public class ListFeedsViewComponent : ViewComponent
+	{
+        private readonly IGetFeeds _getFeeds;
+        private readonly IFeedMapper _mapper;
+
+        public ListFeedsViewComponent(
+            IGetFeeds getFeeds,
+            IFeedMapper mapper)
+        {
+            _getFeeds = getFeeds;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            List<Models.Feed> allFeeds = await _getFeeds.ListFeeds();
+            List<FeedMetadataViewModel> metadata = allFeeds.Select(
+                _mapper.FeedMetadataToViewModel).ToList();
+            return View(metadata);
+        }
+    }
+}
+
---
+++ NewsFlow.Web/ViewModels/FeedViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace NewsFlow.Web.ViewModels
+{
+	public class FeedViewModel
+	{
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+        private FeedViewModel(
+            string name, string description, string link, DateTime lastUpdated)
+        {
+            Name = name;
+            Description = description;
+            Link = link;
+            LastUpdated = lastUpdated;
+        }
+
+        public static FeedViewModel Create(
+            string name, string description, string link, DateTime lastUpdated)
+        {
+            return new FeedViewModel(name, description, link, lastUpdated);
+
+        }
+    }
+}
+
