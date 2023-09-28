a6f540874bb09de2f175d42ec0cbd672438e861d
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-09-28 17:50:50 +0200

    #11: Map feed link to root url for Feed viewmodel

--- NewsFlow.Web/Mapping/FeedMapping/FeedMapper.cs
+++ NewsFlow.Web/Mapping/FeedMapping/FeedMapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using NewsFlow.Data.Models;
 using NewsFlow.Web.ViewModels;
 
@@ -6,10 +7,23 @@ namespace NewsFlow.Web.Mapping.FeedMapping
 {
 	public class FeedMapper : IFeedMapper
 	{
+        private string GetRootDomain(string link)
+        {
+            string urlPattern = @"^(?:https?://)?(?:www\.)?(?<rootDomain>[^/]+)";
+
+            Match match = Regex.Match(
+                link, urlPattern, RegexOptions.IgnoreCase);
+
+            if (match.Success)
+            {
+                return match.Groups["rootDomain"].Value;
+            }
+            return link;
+        }
         public FeedMetadataViewModel FeedMetadataToViewModel(Feed feedModel)
         {
             return FeedMetadataViewModel.Create(
-                feedModel.Name, feedModel.Description);
+                feedModel.Name, feedModel.Description, GetRootDomain(feedModel.Link));
         }
     }
 }
